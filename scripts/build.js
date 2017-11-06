0132c1462e123dbdb67a7ba142e94f01c53a9ce3
avenger339 <jsoirefman@gmail.com>
2017-11-06 06:30:57 -0500

    Map Movement Stuff

--- scripts/build.js
+++ scripts/build.js
@@ -3,15 +3,18 @@ const fs = require('fs-extra')
 const path = require('path')
 const less = require('less')
 const bluebird = require('bluebird')
+const browserify = require('browserify')
+const uglify = require('uglify-js')
+
 const distPath = path.join(__dirname, '../dist')
 
-var html = pug.renderFile(path.join(__dirname, '../src/pug/index.pug'))
-
-createDist().then(() => 
+createDist().then(() =>
   copyAssets()
 ).then(() =>
   processCSS()
-).then(() => 
+).then(() =>
+  browserifyScripts()
+).then(() =>
   renderPugTemplates()
 ).then(() => {
   console.log('Map Generated!')
@@ -19,51 +22,79 @@ createDist().then(() =>
   console.log(err)
 })
 
-function createDist() {
+function createDist () {
   return new Promise((resolve, reject) => {
-    if(fs.existsSync(distPath)) fs.removeSync(distPath)
-    fs.mkdir(distPath).then(() => 
+    if (fs.existsSync(distPath)) fs.removeSync(distPath)
+    fs.mkdir(distPath).then(() =>
       resolve()
-    ).catch((err) => 
+    ).catch((err) =>
       reject(err)
     )
   })
 }
 
-function copyAssets() {
+function copyAssets () {
   return new Promise((resolve, reject) => {
-    fs.copy(path.join(__dirname, '../src/assets'), path.join(__dirname, '../dist/assets')).then(() => 
+    fs.copy(path.join(__dirname, '../src/assets'), path.join(__dirname, '../dist/assets')).then(() =>
       resolve()
-      ).catch((err) => 
+      ).catch((err) =>
         reject()
       )
   })
 }
 
-function processCSS() {
+function processCSS () {
   return new Promise((resolve, reject) => {
-    fs.mkdir(distPath + '/css').then(() => 
+    fs.mkdir(distPath + '/css').then(() =>
       fs.readFile(path.join(__dirname, '../src/less/map.less'), 'utf8')
-    ).then((data) => 
+    ).then((data) =>
       less.render(data)
-    ).then((output) => 
+    ).then((output) =>
       fs.writeFile((distPath + '/css/map.css'), output.css)
-    ).then(() => 
+    ).then(() =>
       resolve()
-    ).catch((err) => 
+    ).catch((err) =>
       reject(err)
     )
   })
 }
 
-function renderPugTemplates() {
+function renderPugTemplates () {
   return new Promise((resolve, reject) => {
-    bluebird.promisify(pug.renderFile)(path.join(__dirname, '../src/pug/index.pug')).then((html) => 
+    bluebird.promisify(pug.renderFile)(path.join(__dirname, '../src/pug/index.pug')).then((html) =>
       fs.writeFile(distPath + '/index.html', html)
-    ).then(() => 
+    ).then(() =>
       resolve()
-    ).catch((err) => 
+    ).catch((err) =>
       reject(err)
     )
+  })
+}
+
+function browserifyScripts () {
+  return new Promise((resolve, reject) => {
+    var b = browserify(null, {standalone: 'Map'})
+    b.add(path.join(__dirname, '../src/scripts/map.js'))
+
+    fs.mkdir(distPath + '/scripts').then(() => {
+      return new Promise((resolve, reject) => {
+        b.bundle((err, buffer) => {
+          if (err) reject(err)
+          resolve(buffer.toString())
+        })
+      })
+    }).then((code) => {
+      return new Promise((resolve, reject) => {
+        var result = uglify.minify(code)
+        if (result.error) reject(err)
+        resolve(result.code)
+      })
+    }).then((code) =>
+      fs.writeFile(distPath + '/scripts/map.js', code)
+    ).then(() =>
+      resolve()
+    ).catch((err) => {
+      reject(err)
+    })
   })
 }
---
+++ src/scripts/map.js
@@ -0,0 +1,59 @@
+var clickLocation
+var curOffset
+
+function handleSwitchClick () {
+  if (this._map.classList.contains('lightworld')) {
+    this._map.classList.remove('lightworld')
+    this._map.classList.add('darkworld')
+    this._map.innerHTML = 'Light World'
+  } else if (this._map.classList.contains('darkworld')) {
+    this._map.classList.remove('darkworld')
+    this._map.classList.add('lightworld')
+    this._map.innerHTML = 'Darkx World'
+  } else {
+    console.log('what?????')
+  }
+}
+
+function handleMapDrag (e) {
+  clickLocation = {x: e.clientX, y: e.clientY}
+  curOffset = {x: this._map.offsetLeft, y: this._map.offsetTop }
+  document.addEventListener('mousemove', this._handleMouseMove)
+}
+
+function handleMapDrop () {
+  console.log('removing mousemove...')
+  document.removeEventListener('mousemove', this._handleMouseMove)
+}
+
+function handleMouseMove (e) {
+  if (this._holder.offsetWidth < 2007) this._map.style.left = (e.clientX - clickLocation.x + curOffset.x < 0 ? (e.clientX - clickLocation.x + curOffset.x > -2007 + this._holder.offsetWidth ? e.clientX - clickLocation.x + curOffset.x : -2007 + this._holder.offsetWidth) : 0) + 'px'
+  if (this._holder.offsetHeight < 2007) this._map.style.top = (e.clientY - clickLocation.y + curOffset.y < 0 ? (e.clientY - clickLocation.y + curOffset.y > -2007 + this._holder.offsetHeight ? e.clientY - clickLocation.y + curOffset.y : -2007 + this._holder.offsetHeight) : 0) + 'px'
+}
+
+// TODO - adjust map if window is resized
+function handleWindowResize () {
+  if (this._holder.offsetWidth >= 2007) this._map.style.left = '50%'
+  if (this._holder.offsetHeight >= 2007) this._map.style.top = '50%'
+  this._map.style.margin = (this._holder.offsetWidth >= 2007 ? '-1003.5px' : '0') + ' 0 0 ' + (this._holder.offsetHeight >= 2007 ? '-1003.5px' : '0')
+}
+
+class Map {
+  constructor () {
+    console.log('ALTTP Map 0.1 Javascript....')
+    this._switch = document.getElementsByClassName('switch')[0]
+    this._holder = document.getElementsByClassName('mapholder')[0]
+    this._map = document.getElementsByClassName('map')[0]
+    this._map.style.left = this._map.style.top = '0'
+
+    this._handleMouseMove = handleMouseMove.bind(this)
+
+    this._switch.addEventListener('click', handleSwitchClick.bind(this))
+
+    this._map.addEventListener('mousedown', handleMapDrag.bind(this))
+    document.addEventListener('mouseup', handleMapDrop.bind(this))
+    // window.addEventListener('resize', handleWindowResize.bind(this))
+  }
+}
+
+module.exports = Map
